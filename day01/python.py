d1c1ca00f0ccb6fe6c4e313222a87d8e2c708392
Sebastian Schwarzer <sschwarzer@outlook.de>
2022-12-01 23:07:00 +0100

    solution to 2

--- day01/python.py
+++ day01/python.py
@@ -1,10 +1,11 @@
 import numpy as np
 import os
 
+
 def solution01():
     with open("./day01/input.txt") as f:
-        example_input=f.read()
-    
+        example_input = f.read()
+
     print(
         np.array(
             [
@@ -14,8 +15,25 @@ def solution01():
         ).max()
     )
 
+
+def solution02():
+    with open("./day01/input.txt") as f:
+        example_input = f.read()
+
+    print(
+        np.sort(
+            [
+                np.array(items.split("\n"), dtype=int).sum()
+                for items in example_input.split("\n\n")
+            ]
+        )[-3:].sum()
+    )
+
+
 def main():
     solution01()
+    solution02()
+
 
 if __name__ == "__main__":
     main()
