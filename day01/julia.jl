f6228fdb689c72d71fc23f2f4084cc4df3c4736f
Sebastian Schwarzer <sschwarzer@outlook.de>
2022-12-04 13:22:26 +0100

    update julia solution

--- day01/julia.jl
+++ day01/julia.jl
@@ -1,14 +1,14 @@
 function solution01(input::String)::Int
     raw_lists = split(input, "\n\n")
-    elves_calories = map(text -> parse.(Int, split(text, "\n")), raw_elves_list)
-    total_calories = sum.(elves_list_parsed)
+    elves_calories = map(text -> parse.(Int, split(text, "\n")), raw_lists)
+    total_calories = sum.(elves_calories)
     return maximum(total_calories)
 end
 
 function solution02(input::String)
     raw_lists = split(input, "\n\n")
-    elves_calories = map(text -> parse.(Int, split(text, "\n")), raw_elves_list)
-    total_calories = sum.(elves_list_parsed)
+    elves_calories = map(text -> parse.(Int, split(text, "\n")), raw_lists)
+    total_calories = sum.(elves_calories)
     return sum(sort(total_calories, rev=true)[1:3])
 end
 
