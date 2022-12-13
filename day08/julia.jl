7d1264c9b324c1c3305e6d61c16189b7caf0b1b4
Sebastian Schwarzer <sschwarzer@outlook.de>
2022-12-13 12:15:35 +0100

    Day 08 Solution 02

--- day08/julia.jl
+++ day08/julia.jl
@@ -27,4 +27,40 @@ function solution01(forest::Matrix{Int})
     return prod(size(forest)) - invisible_counter
 end
 
+function solution02(forest::Matrix{Int})
+    max_scenic_score = 0
+    for row in 2:size(forest)[2]-1
+        for col in 2:size(forest)[1]-1
+            scores = Dict("left"=>0,"right"=>0,"above"=>0,"below"=>0)
+            for left in col-1:-1:1
+                scores["left"] += 1
+                if forest[row,left] >= forest[row,col]
+                    break
+                end
+            end
+            for right in col+1:size(forest)[1]
+                scores["right"] += 1
+                if forest[row,right] >= forest[row,col]
+                    break
+                end
+            end
+            for above in row-1:-1:1
+                scores["above"] += 1
+                if forest[above,col] >= forest[row,col]
+                    break
+                end
+            end
+            for below in row+1:size(forest)[2]
+                scores["below"] += 1
+                if forest[below,col] >= forest[row,col]
+                    break
+                end
+            end
+            max_scenic_score = max(prod(collect(values(scores))), max_scenic_score)
+        end
+    end
+    return max_scenic_score
+end
+
 @assert solution01(get_forest(input)) == 1785
+@assert solution02(get_forest(input)) == 345168
